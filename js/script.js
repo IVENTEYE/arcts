7351f6b771b82eed268732142725159960eb0cf7
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2020-11-28 03:27:28 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -272,3 +272,62 @@ if (document.querySelectorAll('.slider-partners').length > 0) {
 	}
 }());
 
+//======================================================Select===============================================================
+if (document.querySelectorAll('.select').length > 0) {
+	const select = function () {
+		const selectHeader = document.querySelectorAll('.select-header'),
+			selectHeaderIcon = document.querySelectorAll('.select-header__icon'),
+			selectItem = document.querySelectorAll('.select-body__item');
+	
+		const selectToggle = function (e) {
+			this.parentElement.classList.toggle('active');
+		};
+	
+		const selectToggleArrow = function (e) {
+			document.querySelectorAll('.select').forEach(item => {
+				item.addEventListener('click', (e) => {
+					if (item.classList.contains('active')) {
+						selectHeader.forEach(item => {
+							if (item.classList.contains('select active')) {
+								item.classList.toggle('active');
+							}
+						});
+					}
+					const selectArrow = item.querySelector('.select-header__icon');
+					selectArrow.classList.toggle('active');
+				});
+			});
+		};
+	
+		const selectChoose = function () {
+			const text = this.innerHTML,
+				select = this.closest('.select'),
+				currentText = this.closest('.select').querySelector('.select-header__current');
+	
+			currentText.innerHTML = text;
+			select.classList.remove('active');
+			document.querySelector('.select-header__icon').classList.remove('active');
+		};
+	
+		if (document.querySelector('.select')) {
+			document.addEventListener('click', (e) => {
+				if (!e.target.closest('.select')) {
+					document.querySelector('.select').classList.remove('active');
+					document.querySelector('.select-header__icon').classList.remove('active');
+				}
+			});
+		}
+	
+		selectHeader.forEach(item => {
+			item.addEventListener('click', selectToggle);
+		});
+	
+		selectToggleArrow();
+	
+		selectItem.forEach(item => {
+			item.addEventListener('click', selectChoose);
+		});
+	};
+	
+	select();
+}

a8b2572c1e089884786fbe57473f1c2dcfa63cfd
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2020-12-01 22:12:08 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -331,3 +331,63 @@ if (document.querySelectorAll('.select').length > 0) {
 	
 	select();
 }
+
+// $(document).ready(function() {
+
+// 	$('.image-popup').magnificPopup({
+// 		type: 'image',
+// 		closeOnContentClick: true,
+// 		mainClass: 'mfp-img-mobile',
+// 		image: {
+// 			verticalFit: true
+// 		}
+// 	});
+
+// 	$('.image-popup-fit-width').magnificPopup({
+// 		type: 'image',
+// 		closeOnContentClick: true,
+// 		image: {
+// 			verticalFit: false
+// 		}
+// 	});
+
+// 	$('.image-popup-no-margins').magnificPopup({
+// 		type: 'image',
+// 		closeOnContentClick: true,
+// 		closeBtnInside: false,
+// 		fixedContentPos: true,
+// 		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+// 		image: {
+// 			verticalFit: true
+// 		},
+// 		zoom: {
+// 			enabled: true,
+// 			duration: 300 // don't foget to change the duration also in CSS
+// 		}
+// 	});
+
+// });
+
+$(document).ready(function() {
+	$('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: true,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
